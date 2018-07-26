b05b1cfabcf7c6a7daf95407fc28c29e7b40e54b
ostanioni <ostanioni@gmail.com>
2018-07-26 23:49:05 +0700

    quasar added

--- src/main.js
+++ src/main.js
@@ -10,6 +10,19 @@ import App from './App.vue'
 
 import './registerServiceWorker'
 
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import Quasar, * as All from 'quasar'
+
+Vue.use(Quasar, {
+  components: All,
+  directives: All,
+  plugins: All
+})
+
 Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
