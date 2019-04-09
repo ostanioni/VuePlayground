518ea0bdf175678c6a8015a6534f2d66c9413e57
ostanioni <ostanioni@gmail.com>
2019-04-09 22:14:50 +0700

    v0.1.5

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,4 +1,25 @@
-const devMode = process.env.NODE_ENV !== 'production';
+
+const devMode = process.env.NODE_ENV !== 'production'
+let $SOURCEMAP = devMode ? true : false
+ 
+module.exports = {
+ sourceMap: $SOURCEMAP,
+ ident: 'postcss',
+ plugins: [
+   require('postcss-import')(),
+   require('postcss-flexbugs-fixes')(),
+   require('postcss-preset-env')({
+     stage: 3,
+     autoprefixer: { flexbox: 'no-2009', grid: true, },
+     features: {
+     'nesting-rules': true,
+     'color-mod-function': { unresolved: 'warn', }
+     }
+   }),
+   require('cssnano')( {"preset": ["advanced", { "discardComments": {"removeAll": true,} }], } ),
+ ],  
+}
+/*const devMode = process.env.NODE_ENV !== 'production';
 const GridKissParams = {
   grid: true,
   optimize: true,
@@ -14,6 +35,7 @@ module.exports = {
   parser: 'postcss-scss',
   plugins,
 }
+*/
 /**
  * 
  * require('autoprefixer')({grid: true}),
